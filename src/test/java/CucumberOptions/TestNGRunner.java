bdc407ebc1787225fbc5f232ad6f60d4620d019b
Aritra-git4433 <www.aritrasonudey@gmail.com>
2025-04-16 13:31:17 +0530

    Sixth commit

--- src/test/java/CucumberOptions/TestNGRunner.java
+++ src/test/java/CucumberOptions/TestNGRunner.java
@@ -6,7 +6,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-@CucumberOptions(features="src/test/java/features",glue={"StepDefinition","Hooks"},monochrome=true)
+@CucumberOptions(features="src/test/java/features",glue={"StepDefinition","Hooks"},plugin = {
+        "pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+    },monochrome=true)
 public class TestNGRunner extends AbstractTestNGCucumberTests {
 	
 
